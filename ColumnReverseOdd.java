a2d494f5b73f70562c0d90f1a8f5e25ffe99077c
Rk_Barath <rkbarath5717@gmail.com>
2026-04-02 19:39:00 +0530

    Add files via upload

---
+++ ColumnReverseOdd.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class ColumnReverseOdd {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int r=scn.nextInt();
+        int c=scn.nextInt();
+        int arr[][]=new int [r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=scn.nextInt();
+            }
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                 if(j % 2 != 0){
+                    System.out.print(arr[r-1-i][j]+" ");
+                 }
+                 else{
+                    System.out.print(arr[i][j]+" ");
+                 }
+            }
+            System.out.println();
+        }
+    }
+}
